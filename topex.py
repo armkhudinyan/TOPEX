23506714d8ace31c3e8ea0826e09a5014010a24b
armkhudinyan <armkhudinyan@gmail.com>
2023-09-07 17:23:37 +0100

    Fixes topex diagonal distance and angle calculations

--- topex.py
+++ topex.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import numpy as np
 
 class Topex:
@@ -7,14 +8,20 @@ class Topex:
             dem: np.ndarray,
             max_distance: float,
             interval: float,
-            resolution: float
-            ) -> None:
+            *,
+            y_res: float,
+            x_res: Optional[float] = None
+    ) -> None:
         self.dem = dem
         self.max_distance = max_distance
         self.interval = interval
-        self.resolution = resolution
-        self.pixel_per_orth_interval = self.interval / self.resolution
-        self.pixel_per_diag_interval = self.pixel_per_orth_interval / np.sqrt(2)
+        self.y_res = y_res
+        self.x_res = x_res if x_res else self.y_res
+
+        self.pixels_per_interval_y = self.interval / self.y_res
+        self.pixels_per_interval_x = self.interval / self.x_res
+        self.pixels_per_interval_diag_y = self.interval * np.cos(np.pi / 4) / self.y_res
+        self.pixels_per_interval_diag_x = self.interval * np.sin(np.pi / 4) / self.x_res
 
     def _distances(self, pixels_per_inteval: float) -> np.ndarray:
         '''List of distances to calculate the topex for each location '''
@@ -22,33 +29,52 @@ class Topex:
             ) * pixels_per_inteval).round().astype(int)
 
     def _topex_along_axis(self, dem: np.ndarray, axis: int=0) -> np.ndarray:
-        # NOTE: # Axis 2 here refers to any of the diagonal directions
+        # NOTE: Axis 2 here refers to any of the diagonal directions
 
-        distances = self._distances(self.pixel_per_orth_interval
-                        if axis < 2 else self.pixel_per_diag_interval)
-        resolution = (self.resolution
-                        if axis < 2 else self.resolution * np.sqrt(2))
-
-        pad_dist = distances[-1]
-        padded_dem = np.pad(dem, pad_dist)
         slope = np.full(dem.shape, np.pi / 2)
         height, width = dem.shape
+        # NOTE: Distances per interval in number of rows and cols, not in meters
+        distances: np.ndarray = (
+                    self._distances(self.pixels_per_interval_y) if axis == 0
+            else    self._distances(self.pixels_per_interval_x) if axis == 1
+            else    np.array([(row,col) for row,col in zip(
+                    self._distances(self.pixels_per_interval_diag_y),
+                    self._distances(self.pixels_per_interval_diag_x))])
+        )
 
-        for distance in distances:
+        pad_size = (distances[-1] if axis < 2
+            else   (distances[-1][0], distances[-1][1]))
+
+        padded_dem: np.ndarray = (
+                    np.pad(dem, ((pad_size,), (0,))) if axis == 0
+            else    np.pad(dem, ((0,), (pad_size,))) if axis == 1
+            else    np.pad(dem, ((pad_size[0],), (pad_size[1],)))
+        )
+
+        dist_meters = np.arange(self.interval, self.max_distance +
+                                self.interval, self.interval)
+        for i, distance in enumerate(distances): # Number of pixels
             if axis == 0:
-                delta_height = dem - padded_dem[pad_dist + distance: pad_dist +
-                                                distance + height,
-                                                pad_dist: pad_dist + width]
+                # dist_in_meters = distance * self.y_res
+                delta_height = dem - padded_dem[pad_size + distance:
+                                                pad_size + distance + height, :]
             elif axis == 1:
-                delta_height = dem - padded_dem[pad_dist: pad_dist + height,
-                                                pad_dist + distance: pad_dist +
-                                                distance + width]
+                # dist_in_meters = distance * self.x_res
+                delta_height = dem - padded_dem[:, pad_size + distance :
+                                                   pad_size + distance + width]
             else:
-                delta_height = dem - padded_dem[pad_dist + distance: pad_dist +
-                                                distance + height,
-                                                pad_dist + distance: pad_dist +
-                                                distance + width]
-            angle = np.arctan(delta_height / (distance * resolution))
+                num_rows, num_cols = distance
+                pad_size_y, pad_size_x = pad_size
+
+                # dist_in_meters = self.y_res * num_rows / np.cos(np.pi / 4)
+                dist_in_meters = (self.x_res * num_cols / np.sin(np.pi / 4) +
+                                  self.y_res * num_rows / np.cos(np.pi / 4)) / 2
+
+                delta_height = dem - padded_dem[
+                    pad_size_y + num_rows : pad_size_y + num_rows + height,
+                    pad_size_x + num_cols : pad_size_x + num_cols + width]
+
+            angle = np.arctan(delta_height / dist_meters[i])
             slope = np.minimum(slope, angle)
 
         return slope * -1
